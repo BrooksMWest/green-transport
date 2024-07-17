6bee97ac9a05fde1096fca4bc8d45800de32996e
Skeo <skeo051600@gmail.com>
2024-07-16 19:58:12 -0500

    Add routing to navbar

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Faq() {
+  return (
+    <div className="text-center my-4">
+      <div className="d-flex flex-wrap">
+        <h1>faq.text</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Faq;
---
+++ pages/startRide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function StartRide() {
+  return (
+    <div className="text-center my-4">
+      <div className="d-flex flex-wrap">
+        <h1>sr.text</h1>
+      </div>
+    </div>
+  );
+}
+
+export default StartRide;
