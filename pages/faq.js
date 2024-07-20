934fe5219fa4306d1f474dc509c910628c45ba36
Skeo <skeo051600@gmail.com>
2024-07-20 07:52:19 -0500

    finish faq

--- pages/faq.js
+++ pages/faq.js
@@ -3,8 +3,26 @@ import React from 'react';
 function Faq() {
   return (
     <div className="text-center my-4">
-      <div className="d-flex flex-wrap">
-        <h1>faq.text</h1>
+      <div className="d-flex flex-wrap" id="faq-container">
+        <h1 id="faq-header">ABOUT US</h1>
+        <h5 id="faq-text">We care about the environment + your money!</h5>
+      </div>
+      <div className="d-flex flex-wrap" id="faq-text-container">
+        <h5 id="faq-about">
+          Welcome to Green Transport, where urban mobility meets sustainability. At Green Transport, we are dedicated to revolutionizing the way people navigate cities with our innovative and eco-friendly scooters. Our mission is to provide efficient, affordable, and environmentally conscious transportation solutions that make a positive impact on our planet.
+          <br />
+          <br />
+          Our journey began with a simple idea: to reduce the carbon footprint of daily commutes and create a cleaner, greener urban environment. We believe that small changes can lead to big impacts, and our scooters are designed with this philosophy in mind. Each of our scooters is equipped with cutting-edge technology and powered by clean energy, ensuring a smooth and emission-free ride.
+          <br />
+          <br />
+          At Green Transport, we take pride in our commitment to sustainability. From the materials we use to the processes we implement, every aspect of our business is geared towards minimizing environmental impact. Our scooters are not only a convenient mode of transportation but also a statement of our dedication to preserving the environment for future generations.
+          <br />
+          <br />
+          We are more than just a scooter company; we are a community of like-minded individuals who share a vision of a greener world. Our team is passionate about innovation, quality, and customer satisfaction. We strive to create products that not only meet the needs of urban commuters but also inspire a shift towards more sustainable living.
+          <br />
+          <br />
+          Join us in our mission to transform urban transportation and make a positive difference in the world. With Green Transport, you’re not just choosing a scooter—you’re choosing a cleaner, brighter future. Together, we can ride towards a more sustainable tomorrow.
+        </h5>
       </div>
     </div>
   );
