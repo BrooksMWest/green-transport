52e771f594cfaad0500628142838e5c7d7e62911
Brooks West <brooksmwest@hotmail.com>
2024-07-22 19:00:43 -0500

    welcome page looking ok and pulling google auth data but neets styling and link functionality

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import RideHistory from './rideHistory';
 import StartRide from './startRide';
+import { useAuth } from '../utils/context/authContext';
 
 function Welcome() {
+  const { user } = useAuth();
+
   return (
-    <div className="text-center my-4">
-      <div className="d-flex flex-wrap">
-        <Button type="button" size="lg" className="copy-btn" onClick={StartRide}>
-          Start Ride
-        </Button>
-        <Button type="button" size="lg" className="copy-btn" onClick={RideHistory}>
-          Ride History
-        </Button>
+    <>
+      <div className="text-center my-4">
+        <div className="d-flex flex-wrap" style={{ position: 'absolute', top: 100, right: 20 }}>
+          <Image
+            src={user.photoURL}
+            alt="User Avatar"
+            roundedCircle
+            style={{ width: 100, height: 100 }}
+          />
+          <div>
+            <h1>Welcome, {user.displayName} {user.photURL}</h1>
+          </div>
+        </div>
+        <div>
+          <Button type="button" size="lg" className="copy-btn" onClick={StartRide}>
+            Start Ride
+          </Button>
+          <Button type="button" size="lg" className="copy-btn" onClick={RideHistory}>
+            Ride History
+          </Button>
+
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
