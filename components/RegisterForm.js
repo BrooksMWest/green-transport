5c14553220ce8466d6854402176822e091d67c1f
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-07-29 20:41:33 -0400

    added in form for register user

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -6,9 +6,19 @@ import { registerUser } from '../utils/auth'; // Update with path to registerUse
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    username: '',
+    name: '',
+    email: '',
     uid: user.uid,
   });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,11 +27,22 @@ function RegisterForm({ user, updateUser }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
+        <Form.Label>Enter Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      {/* <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Register User</Form.Label>
         <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
         <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-      </Form.Group>
+      </Form.Group> */}
       <Button variant="primary" type="submit">
         Register Membership
       </Button>
@@ -31,6 +52,9 @@ function RegisterForm({ user, updateUser }) {
 
 RegisterForm.propTypes = {
   user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
     uid: PropTypes.string.isRequired,
   }).isRequired,
   updateUser: PropTypes.func.isRequired,
